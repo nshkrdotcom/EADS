ea0f2b77a8626b4a37c610090362b3c01cad57bd
nshkrdotcom <>
2024-12-02 13:22:34 -1000

    work on mypy tests

--- nlp/__init__.py
+++ nlp/__init__.py
@@ -1,5 +1,5 @@
 """Natural Language Processing module for EADS."""
 
-from nlp.nlp_service import app, encode_text, analyze_pattern
+from nlp.nlp_service import analyze_pattern, app, encode_text
 
 __all__ = ["app", "encode_text", "analyze_pattern"]
